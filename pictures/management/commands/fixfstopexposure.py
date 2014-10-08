bd56f421359d003016f5bb2bd033b59af28bbf91
Eric Floehr <eric@intellovations.com>
2014-10-08 11:34:23 -0400

    Additional processing comments

--- pictures/management/commands/fixfstopexposure.py
+++ pictures/management/commands/fixfstopexposure.py
@@ -4,6 +4,9 @@ import pictures.tasks as tasks
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        self.stdout.write("Loading pictures...")
         pictures = Picture.objects.all()
+        self.stdout.write("Loaded {0} pictures".format(len(pictures)))
         for picture in pictures:
             tasks.fix_fstop_exposure.delay(picture.id)
+        self.stdout.write("Complete")
