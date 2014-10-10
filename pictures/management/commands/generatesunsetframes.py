c3c7e1c0a4c25a422a282f924c66e11a8d92e537
Eric Floehr <eric@intellovations.com>
2014-10-10 00:18:16 +0000

    bug fixes

--- pictures/management/commands/generatesunsetframes.py
+++ pictures/management/commands/generatesunsetframes.py
@@ -7,7 +7,7 @@ from sky import est
 class Command(BaseCommand):
     def handle(self, *args, **options):
         # Make it easy, start on August 1 and get all normalized times to cycle through
-        start = datetime(2013,8,1,0,0,0).replace(tzinfo=est)
+        start = datetime(2013,8,1,tzinfo=est)
         # From 3 hours before to 1 hour after sunset
-        for sequence_no, seconds_before_sunset in enumerate(range(3*60*60, -1*60*60, -1)):
+        for sequence_no, seconds_before_sunset in enumerate(range(5*60*60, -2*60*60, -10)):
             make_sunset_synchro_frame.delay('/var/tlwork/sunset', sequence_no+1, start, seconds_before_sunset, 20, 1920, True, 140)
