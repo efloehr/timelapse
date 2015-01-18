0e3c4214ac5b639b79254a2c9c1bb08b27a1cb8b
Eric Floehr <eric@intellovations.com>
2015-01-18 17:20:00 -0500

    management command for insert

---
+++ image/management/commands/insert_images.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+import fileinput
+from image.models import Normal
+import image.tasks as tasks
+
+class Command(BaseCommand):
+    args = 'None'
+    help = 'Imports a set of timestamped timelapse pictures'
+    option_list = BaseCommand.option_list + (
+            make_option('--nocheck',
+                action='store_false',
+                dest='check',
+                help='Don\'t check that picture already exists in the database'
+                ),
+            make_option('--check',
+                action='store_true',
+                dest='check',
+                default=True,
+                help='Check if picture already exists in the database (default)'
+                )
+            )
+    
+    def handle(self, *args, **options):
+        # Make sure our normals are up-to-date
+        Normal.update_normals()
+        
+        # Go through the list of files
+        for line in fileinput.input():
+            # Remove spaces and new lines
+            line = line.strip()
+            insert.delay(filename)
+        
+
